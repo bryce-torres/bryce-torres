514cee799e8eece6ce3148162752c35706a466a0
bryce-torres <justinbrycetorres@gmail.com>
2025-10-12 18:12:43 +0800

    added a clock

--- script.js
+++ script.js
@@ -53,6 +53,22 @@ function showTask(){
     listContainer.innerHTML = localStorage.getItem("data");
 }
 
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
 //shows the task 
 showTask();
 // Get the element with id="defaultOpen" and click on it
